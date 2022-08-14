0abb11a137e9acc82cf96ac6bf12bcddc12f8fbf
shobhitgarg420 <goforshobhit@gmail.com>
2022-08-14 16:48:13 +0530

    first time commit for Iterator Example

---
+++ iteratorExample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class iteratorExample {
+
+    public static void main(String[] args) {
+        List<Integer> numberList=new ArrayList<Integer>();
+        numberList.add(1);
+        numberList.add(2);
+        numberList.add(3);
+        numberList.add(4);
+        iterate(numberList);
+    }
+
+
+    public static void iterate (List<Integer> numberList) {
+        ListIterator<Integer> it = numberList.listIterator(1);
+        System.out.println(it.next());
+        it.next();
+        it.previous();
+        System.out.println(it.next());
+    }
+}
