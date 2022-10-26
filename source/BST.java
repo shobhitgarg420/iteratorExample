fc9a946b2c9d6bec3c3a4d66446e570049c586db
shobhitgarg420 <goforshobhit@gmail.com>
2022-10-26 14:57:39 +0530

    created a package

--- source/BST.java
+++ source/BST.java
@@ -6,4 +6,25 @@ public class BST {
     public BST() {
         this.root = null;
     }
+
+    public void preOrder(Node root){
+        if (root==null)
+            return;
+
+        System.out.print(root.data + " ");
+
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+    public void inOrder(Node root){
+        if (root==null)
+            return;
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+
+    }
 }
